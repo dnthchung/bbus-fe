a643373f7588a7c78e2fb5bb7325adb639b7a3c9
dnthchung <chungdthe176077@fpt.edu.vn>
2025-02-13 01:31:02 +0700

    mdf
    Co-Authored-By: Truong Thi Phuong Thao <137491155+ThaoTTP123@users.noreply.github.com>
    Co-Authored-By: Enric Van <122770211+enricVan@users.noreply.github.com>

--- fe/src/routes/(auth)/sign-up.lazy.tsx
+++
@@ -1,8 +0,0 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
-import { withGuestRoute } from '@/guards/withGuestRoute'
-import SignUp from '@/features/auth/sign-up'
-
-export const Route = createLazyFileRoute('/(auth)/sign-up')({
-  // component: SignUp,
-  component: withGuestRoute(SignUp),
-})
--- fe/src/routes/_authenticated/index.tsx
+++
@@ -1,7 +0,0 @@
-//path : src/routes/_authenticated/index.tsx
-import { createFileRoute } from '@tanstack/react-router'
-import Dashboard from '@/features/dashboard'
-
-export const Route = createFileRoute('/_authenticated/')({
-  component: Dashboard,
-})
