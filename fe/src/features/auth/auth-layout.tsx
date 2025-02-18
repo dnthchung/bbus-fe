bfc2ed6cf2db7275ccf60f6066ae1c8bf8de886b
dnthchung <chungdthe176077@fpt.edu.vn>
2025-02-18 10:05:08 +0700

    refactor: update index.html and auth-layout to use BBus branding and logo
    Co-Authored-By: Truong Thi Phuong Thao <137491155+ThaoTTP123@users.noreply.github.com>

--- fe/src/features/auth/auth-layout.tsx
+++ fe/src/features/auth/auth-layout.tsx
@@ -1,3 +1,6 @@
+//path : src/features/auth/auth-layout.tsx
+import BBusLogo from '@/assets/bbus-logo.svg'
+
 interface Props {
   children: React.ReactNode
 }
@@ -7,7 +10,7 @@ export default function AuthLayout({ children }: Props) {
     <div className='container grid h-svh flex-col items-center justify-center bg-primary-foreground lg:max-w-none lg:px-0'>
       <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8'>
         <div className='mb-4 flex items-center justify-center'>
-          <svg
+          {/* <svg
             xmlns='http://www.w3.org/2000/svg'
             viewBox='0 0 24 24'
             fill='none'
@@ -18,8 +21,10 @@ export default function AuthLayout({ children }: Props) {
             className='mr-2 h-6 w-6'
           >
             <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-          </svg>
-          <h1 className='text-xl font-medium'>Shadcn Admin</h1>
+          </svg> */}
+
+          <img src={BBusLogo} alt='BBus Logo' className='mr-2 h-6 w-6' />
+          <h1 className='text-xl font-medium'>BBus Admin</h1>
         </div>
         {children}
       </div>
